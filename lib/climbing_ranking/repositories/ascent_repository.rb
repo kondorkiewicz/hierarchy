712e59cee2252d6d121a3d7f71de4d903da8e707
kondor <konrad.ociepka@netguru.pl>
2019-11-27 20:39:06 +0100

    Add associations

--- lib/climbing_ranking/repositories/ascent_repository.rb
+++ lib/climbing_ranking/repositories/ascent_repository.rb
@@ -1,2 +1,6 @@
 class AscentRepository < Hanami::Repository
+  associations do
+    belongs_to :user
+    belongs_to :line
+  end
 end
--- lib/climbing_ranking/repositories/line_repository.rb
+++ lib/climbing_ranking/repositories/line_repository.rb
@@ -1,2 +1,6 @@
 class LineRepository < Hanami::Repository
+  associations do
+    has_many :ascents
+    has_many :users, through: :ascents
+  end
 end
